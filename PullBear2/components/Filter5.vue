77e8233994b1b7cbcc897ed36ec5183bea6fd5e9
Mert Yıldırım <148050600+mertyldrm29@users.noreply.github.com>
2025-01-04 21:15:55 +0300

    Filter5.vue eklendi ve Products.vue güncellendi.
    Jackets kategorisindeki ürünleri filtrelemeye yarayan component eklendi. Products bu componente göre düzenlendi.

---
+++ PullBear2/components/Filter5.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="filter-buttons">
+      <button 
+        :class="{ active: subJackets === 'all' }" 
+        @click="applyFilter('all')"
+      >
+        Hepsi
+      </button>
+      <button 
+        :class="{ active: subJackets === 'ceket' }" 
+        @click="applyFilter('ceket')"
+      >
+        Ceket
+      </button>
+      <button 
+        :class="{ active: subJackets === 'mont' }" 
+        @click="applyFilter('mont')"
+      >
+        Mont
+      </button>
+      <button 
+        :class="{ active: subJackets === 'kaban' }" 
+        @click="applyFilter('kaban')"
+      >
+        Kaban
+      </button>
+    
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        subJackets: 'all', // Varsayılan olarak "Tümünü Gör"
+      };
+    },
+    methods: {
+      applyFilter(filter5) {
+        this.subJackets = filter5;
+        this.$emit('filter-changed', filter5); // Parent component'e filtreyi gönderir
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .filter-buttons {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 20px;
+  }
+  
+  button {
+    padding: 10px;
+    border: none;
+    cursor: pointer;
+    background-color: #f1f1f1;
+    font-size: 14px;
+    border-radius: 100px;
+  }
+  
+  button.active {
+    background-color: #000;
+    color: white;
+  }
+  
+  
+  </style>
+  
