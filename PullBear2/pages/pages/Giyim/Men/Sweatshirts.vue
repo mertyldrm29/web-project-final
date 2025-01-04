e4348d5cc65b839ae67df120165aaad6f4ea0dcc
Mert Yıldırım <148050600+mertyldrm29@users.noreply.github.com>
2025-01-04 16:53:20 +0300

    Pages klasöründeki bütün dosyalar güncellendi.
    Bu klasörde ürün gösterilen bütün sayfalarda filter2'den dolayı değişiklik yapıldı.

---
+++ PullBear2/pages/pages/Giyim/Men/Sweatshirts.vue
@@ -0,0 +1,68 @@
+<template>
+     <div class="logo-container">
+        <NuxtLink to="/">
+      <img src="https://static.pullandbear.net/2/static2/img/headLogo/logo_pull_black_new.svg" alt="Logo" class="logo" />
+    </NuxtLink>
+    </div>
+    <Menu />
+    <div class="category-page">
+        <h1>Erkek Sweatshirt Koleksiyonu</h1>
+    </div>
+    <div class="products-section">
+        <Products :defaultGender="'men'" :defaultCategory="'sweatshirts'" />
+    </div>
+    <Button />
+    <Sepet />
+    <Footer />
+</template>
+
+<script>
+import Menu from '../../components/Menu.vue';
+import Products from '../../components/Products.vue';
+import Button from '../../components/Button.vue';
+import Sepet from '../../components/Sepet.vue';
+import Footer from '../../components/Footer.vue';
+
+export default {
+    name: 'MenSweatshirtsPage',
+    components: {
+        Menu,
+        Products,
+        Button,
+        Sepet,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+.category-page {
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    margin-top: -15px;
+}
+
+h1 {
+    font-size: em;
+    margin-bottom: 10px;
+}
+
+.products-section {
+    width: 100%;
+    margin-top: 20px;
+}
+.logo-container {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.logo {
+  max-width: 200px;
+}
+.menu-container {
+  margin-top: -20px;
+  position: relative;
+}
+</style>
