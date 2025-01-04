423e876a329988c8de7a65b9ba7e90bea9095206
Mert Yıldırım <148050600+mertyldrm29@users.noreply.github.com>
2025-01-05 02:37:16 +0300

    firebase.js güncellendi.
    Kullanıcının sayfa değiştirdiğinde hesabından çıkış yapması hatasını düzelten değişiklikler yapıldı.

--- PullBear2/plugins/firebase.js
+++ PullBear2/plugins/firebase.js
@@ -3,33 +3,30 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAJ_ScGicZUcOzxufWWp8kG92I6LS_F5l4",
-  authDomain: "web-project-16f2e.firebaseapp.com",
-  projectId: "web-project-16f2e",
-  storageBucket: "web-project-16f2e.firebasestorage.app",
-  messagingSenderId: "974775517489",
-  appId: "1:974775517489:web:a47dd6010e22a301087010",
-  measurementId: "G-PS7BP1WV3P"
-};
-
-// Initialize Firebase
-let app;
-let db;
-let auth;
-
-if (typeof window !== 'undefined') {
-  app = initializeApp(firebaseConfig);
-  db = getFirestore(app);
-  auth = getAuth(app);
-}
-
 export default defineNuxtPlugin(nuxtApp => {
-  return {
-    provide: {
-      db: db,
-      auth: auth
+  const firebaseConfig = {
+    apiKey: "AIzaSyAJ_ScGicZUcOzxufWWp8kG92I6LS_F5l4",
+    authDomain: "web-project-16f2e.firebaseapp.com",
+    projectId: "web-project-16f2e",
+    storageBucket: "web-project-16f2e.firebasestorage.app",
+    messagingSenderId: "974775517489",
+    appId: "1:974775517489:web:a47dd6010e22a301087010",
+    measurementId: "G-PS7BP1WV3P"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  // Kullanıcı durumunu localStorage'da sakla
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
     }
-  }
+  });
+
+  nuxtApp.provide('db', db);
+  nuxtApp.provide('auth', auth);
 });
